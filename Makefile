7daac09eb4ea28a0bbec4208fa9329a2b22c5ef3
RENATO RIBEIRO DA SILVA <rrs17@inf.ufpr.br>
2017-11-09 19:13:55 -0200

    tentando imprimir o mapa na janela do jogo

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: lerMapa
 
 lerMapa: lerMapa.c 
-	gcc lerMapa.c -lncurses
+	gcc lerMapa.c -Wall -lncurses -g
 
 clean:
 	-rm *.o
--- lerMapa.c
+++ lerMapa.c
@@ -2,30 +2,67 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-#define COLMAX 160
+#define COLMAX 160+1
 #define LINMAX 50
+#define ALT_SCORE 3
 
-void desenharBordas(WINDOW *screen) {
-  box( screen, 0, 0 ); // sets default borders for the window
+void desenharBordas(WINDOW *janela) {
+  box( janela, 0, 0 ); // desenha as bordas da janela indicada
+}
+
+char **lerMapa(char **mapa,char *mapaNome){ //matriz q vai guardar o mapa, nome do arquivo a ser lido
+	char c;
+
+	FILE *mapaArq = fopen(mapaNome,"r"); //abro o arquivo do mapa
+	if (!mapaArq){
+ 		endwin(); //finaliza o ncurses para printar o erro
+		perror("Arquivo nao aberto: "); //saber se arquivo foi aberto
+		exit(1);
+	}
+
+	mapa = malloc (LINMAX * sizeof(int*)); // aloca espaço de memoria para a matriz do mapa
+	for (int i = 0; i < LINMAX; ++i)
+	{
+		mapa[i] = malloc (COLMAX * sizeof(int));
+	}
+
+  	for (int i = 0; i < LINMAX; ++i)//le os caracteres do arquivo e grava na matriz
+	{
+		for (int j = 0; j < COLMAX; ++j)
+		{
+			c=getc(mapaArq);
+			mapa[i][j] = c;
+		}
+	}
+
+	return(mapa);
+}
+
+void imprimeMapa(char **mapa, WINDOW *janela){
+    mvwprintw(janela, 1, 3, "Coins:");
 }
 
 int main(int argc, char *argv[]) {
-  int gameOver = 0;
-  int xAtual, yAtual, novoX, novoY;
-  int alturaScore = 3;
+	int gameOver = 0; 
+	int xAtual, yAtual, novoX, novoY; //cordenadas
 
-  initscr();
-  noecho();
-  curs_set(FALSE);
+	char *mapa1 = "map.h";
+	char **mapa = NULL;
+  
+	initscr();
+	noecho();
+	curs_set(0);
 
-  // set up initial windows
-  getmaxyx(stdscr, yAtual, xAtual);
+	mapa = lerMapa(mapa,mapa1);
 
-  WINDOW *janelaPontos = newwin(alturaScore, xAtual, 0, 0);//altura,largura,posX,posY
-  WINDOW *janelaJogo = newwin(yAtual - alturaScore, xAtual, alturaScore, 0);	
+	getmaxyx(stdscr, yAtual, xAtual); //pega o max x e y da janela atual no terminal
 
-  desenharBordas(janelaJogo);
-  desenharBordas(janelaPontos);
+	WINDOW *janelaScore = newwin(ALT_SCORE, xAtual, 0, 0); //altura,largura,posX,posY
+	WINDOW *janelaJogo = newwin(yAtual - ALT_SCORE, xAtual, ALT_SCORE, 0);	
+
+	desenharBordas(janelaScore);
+	desenharBordas(janelaJogo);
+	// imprimeMapa(janelaJogo);
 
   while(!gameOver){
     getmaxyx(stdscr, novoY, novoX);
@@ -34,31 +71,32 @@ int main(int argc, char *argv[]) {
       xAtual = novoX;
       yAtual = novoY;
 
-      wresize(janelaPontos, alturaScore, xAtual);
-      wresize(janelaJogo, yAtual - alturaScore, xAtual);
-      mvwin(janelaJogo, yAtual + alturaScore, 0);
+      wresize(janelaScore, ALT_SCORE, xAtual);
+      wresize(janelaJogo, yAtual - ALT_SCORE, xAtual);
+      mvwin(janelaJogo, yAtual + ALT_SCORE, 0);
 
       wclear(stdscr);
       wclear(janelaJogo);
-      wclear(janelaPontos);
+      wclear(janelaScore);
 
       desenharBordas(janelaJogo);
-      desenharBordas(janelaPontos);
+      desenharBordas(janelaScore);
+	  // imprimeMapa(janelaJogo);
     }
 
-    // draw to our windows
-    mvwprintw(janelaPontos, 1, 1, "Hearts:");
-    mvwprintw(janelaPontos, 1, 8, "00");
-    mvwprintw(janelaPontos, 1, 12, "Coins:");
-    mvwprintw(janelaPontos, 1, 18, "00");
-    mvwprintw(janelaPontos, 1, 22, "Keys:");
-    mvwprintw(janelaPontos, 1, 27, "00");
+    // infos score
+    mvwprintw(janelaScore, 1, 1, "Hearts:");
+    mvwprintw(janelaScore, 1, 8, "00");
+    mvwprintw(janelaScore, 1, 12, "Coins:");
+    mvwprintw(janelaScore, 1, 18, "00");
+    mvwprintw(janelaScore, 1, 22, "Keys:");
+    mvwprintw(janelaScore, 1, 27, "00");
 
     mvwprintw(janelaJogo, 1, 1, "janelaJogo");
 
     // refresh each window
+    wrefresh(janelaScore);
     wrefresh(janelaJogo);
-    wrefresh(janelaPontos);
   }
 
   endwin();
